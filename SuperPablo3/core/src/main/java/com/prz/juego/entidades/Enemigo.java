8700bfd5be7582d4c8e65640295b18e8d245b816
Octa-Romero <166446204+Octa-Romero@users.noreply.github.com>
2026-08-04 22:56:35 -0300

    Mal subido los componentes del commit

--- SuperPablo3/core/src/main/java/com/prz/juego/entidades/Enemigo.java
+++
@@ -1,4 +0,0 @@
-package com.prz.juego.entidades;
-
-public class Enemigo {
-}
--- SuperPablo3/core/src/main/java/com/prz/juego/utilidades/Config.java
+++
@@ -1,7 +0,0 @@
-package com.prz.juego.utilidades;
-
-public class Config {
-
-    public static final int ANCHO = 1080;
-    public static final int ALTO = 650;
-}
