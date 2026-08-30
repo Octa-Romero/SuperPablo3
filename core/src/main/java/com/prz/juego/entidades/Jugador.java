693381c8af4de661d623de1e9d19ccc948723da9
Santiago Papale <166446295+SPapale@users.noreply.github.com>
2026-08-30 16:44:33 -0300

    feat: agregar animacion de caminar a Walter

--- core/src/main/java/com/prz/juego/entidades/Jugador.java
+++ core/src/main/java/com/prz/juego/entidades/Jugador.java
@@ -1,48 +1,85 @@
 package com.prz.juego.entidades;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
 import com.prz.juego.utilidades.Entrada;
 import com.prz.juego.utilidades.Render;
-import com.badlogic.gdx.math.Rectangle;
-
-import java.awt.*;
 
 public class Jugador {
 
     public Texture textura;
     public Sprite sprite;
+
     private float x, y;
     private float ancho, alto;
+
     private int velocidadX = 200;
     private int velocidadY = 0;
+
     private final int gravedad = 400;
+
     private boolean enSuelo = true;
+
     private Rectangle bounds = new Rectangle();
+    private Texture[] texturasCaminar;
+    private TextureRegion[] framesCaminar;
+    private Animation<TextureRegion> animacionCaminar;
+    private float tiempoAnimacion = 0f;
+
+    private boolean mirandoDerecha = true;
+
 
     public Jugador(float x, float y, float ancho, float alto)
     {
-        textura = new Texture("walter.png");
+
+        textura = new Texture("personaje/walter/13.png");
+
+
+        texturasCaminar = new Texture[3];
+        framesCaminar = new TextureRegion[3];
+
+        texturasCaminar[0] = new Texture("personaje/walter/14.png");
+        texturasCaminar[1] = new Texture("personaje/walter/15.png");
+        texturasCaminar[2] = new Texture("personaje/walter/16.png");
+
+        for (int i = 0; i < 3; i++)
+        {
+            framesCaminar[i] = new TextureRegion(texturasCaminar[i]);
+        }
+
+
+        animacionCaminar = new Animation<>(0.12f, framesCaminar);
+        animacionCaminar.setPlayMode(Animation.PlayMode.LOOP);
+
+
         sprite = new Sprite(textura);
+
         this.x = x;
         this.y = y;
         this.ancho = ancho;
         this.alto = alto;
+
         this.bounds = new Rectangle(x, y, ancho, alto);
+
         sprite.setPosition(x, y);
         sprite.setSize(ancho, alto);
     }
+
 
     public void dibujar()
     {
         sprite.draw(Render.batch);
     }
 
+
     public void update(Entrada entrada, float delta)
     {
+        boolean caminando = false;
+
+        // Gravedad
         if (!enSuelo)
         {
             velocidadY -= gravedad * delta;
@@ -50,77 +87,159 @@ public class Jugador {
 
         y += velocidadY * delta;
 
+
+        // Movimiento izquierda
         if (entrada.mueveIzquierda())
         {
             x -= velocidadX * delta;
+
+            caminando = true;
+            mirandoDerecha = false;
         }
 
+
+        // Movimiento derecha
         if (entrada.mueveDerecha())
         {
             x += velocidadX * delta;
+
+            caminando = true;
+            mirandoDerecha = true;
         }
 
-        if(entrada.mueveArriba())
+
+        // Salto
+        if (entrada.mueveArriba())
         {
             saltar();
         }
 
+
+        // Animación
+        if (caminando)
+        {
+            tiempoAnimacion += delta;
+
+            TextureRegion frameActual =
+                animacionCaminar.getKeyFrame(tiempoAnimacion);
+
+            sprite.setRegion(frameActual);
+
+            // Dirección
+            if (mirandoDerecha)
+            {
+                sprite.setFlip(false, false);
+            }
+            else
+            {
+                sprite.setFlip(true, false);
+            }
+        }
+        else
+        {
+            // Walter quieto
+            tiempoAnimacion = 0f;
+
+            sprite.setRegion(textura);
+
+            if (mirandoDerecha)
+            {
+                sprite.setFlip(false, false);
+            }
+            else
+            {
+                sprite.setFlip(true, false);
+            }
+        }
+
+
+        // Actualizar posición
         sprite.setPosition(x, y);
         bounds.setPosition(x, y);
     }
 
-    private void saltar(){
-        if (enSuelo) {
+
+    private void saltar()
+    {
+        if (enSuelo)
+        {
             velocidadY = 350;
             enSuelo = false;
         }
     }
+
 
     public void setEnSuelo(boolean valor)
     {
         this.enSuelo = valor;
     }
 
-    public float getX() {
+
+    public float getX()
+    {
         return x;
     }
 
-    public float getY() {
+
+    public float getY()
+    {
         return y;
     }
+
 
     public Rectangle getBounds()
     {
         return this.bounds;
     }
 
-    public int getVelocidadX() {
+
+    public int getVelocidadX()
+    {
         return velocidadX;
     }
 
-    public int getVelocidadY() {return velocidadY;}
 
-    public void setVelocidadX(int valor) {this.velocidadX = valor;}
+    public int getVelocidadY()
+    {
+        return velocidadY;
+    }
 
-    public void setVelocidadY(int valor) {this.velocidadY = valor;}
+
+    public void setVelocidadX(int valor)
+    {
+        this.velocidadX = valor;
+    }
+
+
+    public void setVelocidadY(int valor)
+    {
+        this.velocidadY = valor;
+    }
+
 
     public void setX(float x)
     {
         this.x = x;
+
         sprite.setX(x);
         bounds.setX(x);
     }
 
+
     public void setY(float y)
     {
         this.y = y;
+
         sprite.setY(y);
         bounds.setY(y);
     }
 
-    public float getAncho() {
+
+    public float getAncho()
+    {
         return this.ancho;
     }
+
 
     public float getAlto()
     {
---
+++ core/src/main/java/com/prz/juego/utilidades/Colisiones.java
@@ -0,0 +1,41 @@
+package com.prz.juego.utilidades;
+
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Rectangle;
+import com.prz.juego.entidades.Jugador;
+
+import java.util.ArrayList;
+
+public class Colisiones {
+
+    private ArrayList<Rectangle> colisiones;
+
+    public Colisiones(TiledMap tiledMap) {
+        colisiones = new ArrayList<>();
+        cargarColisiones(tiledMap);
+    }
+
+    private void cargarColisiones(TiledMap tiledMap){
+        for(MapObject object : tiledMap.getLayers().get("colisiones").getObjects()){
+            if(object instanceof RectangleMapObject){
+                Rectangle rect = ((RectangleMapObject) object).getRectangle();
+                colisiones.add(rect);
+            }
+        }
+    }
+
+    public void chequearColision(Jugador jugador) {
+        for (Rectangle suelo : colisiones) {
+            if (jugador.getBounds().overlaps(suelo)) {
+                jugador.setY(suelo.y + suelo.height);
+                break;
+            }
+        }
+    }
+
+    public ArrayList<Rectangle> getColisiones() {
+        return colisiones;
+    }
+}
